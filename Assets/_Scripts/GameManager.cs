ef4ae26e56c77372abb54e3b8492257ce9ab11f1
Amanda Reyes <amandabreyesr@gmail.com>
2025-12-11 14:18:26 -0600

    Pause and play game implemented

--- Assets/_Scripts/GameManager.cs
+++ Assets/_Scripts/GameManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
@@ -22,10 +23,15 @@ public class GameManager : MonoBehaviour
         GameOver,
     }
 
+    [SerializeField] private TextMeshProUGUI scoreTMP;
+    [SerializeField] private TextMeshProUGUI lostFoodTMP;
+    [SerializeField] private TextMeshProUGUI gameOverTMP;
+    [SerializeField] private TextMeshProUGUI gamePausedTMP;
+    [SerializeField] private RawImage pauseOrPlayImage;
+    [SerializeField] private Sprite[] pauseAndPlaySprites; //0: Pause, 1: Play
+
     public List<GameObject> targetPrefabs;
-    public TextMeshProUGUI scoreTMP;
-    public TextMeshProUGUI lostFoodTMP;
-    public TextMeshProUGUI gameOverTMP;
+
 
     [HideInInspector] public GameState currentGameState = GameState.Loading;
     [HideInInspector] public bool gameOver = false;
@@ -43,7 +49,9 @@ public class GameManager : MonoBehaviour
 
     void Start()
     {
+        pauseOrPlayImage.texture = pauseAndPlaySprites[0].texture; //Pausa
         gameOverTMP.gameObject.SetActive(false);
+        gamePausedTMP.gameObject.SetActive(false);
         currentGameState = GameState.Playing;
         StartCoroutine(SpawnTarget());
         Score = 0;
@@ -97,4 +105,19 @@ public class GameManager : MonoBehaviour
         gameOverTMP.gameObject.SetActive(true);
     }
 
+    public void PauseGame()
+    {
+        if (currentGameState == GameState.Playing)
+        {
+            currentGameState = GameState.Paused;
+            pauseOrPlayImage.texture = pauseAndPlaySprites[1].texture; //Play
+            Time.timeScale = 0f;
+        }
+        else if (currentGameState == GameState.Paused)
+        {
+            currentGameState = GameState.Playing;
+            pauseOrPlayImage.texture = pauseAndPlaySprites[0].texture; //Pausa
+            Time.timeScale = 1f;
+        }
+    }
 }
